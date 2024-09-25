0394b7f4893bba75cf167c9518d405507e3e1e6d
pertodomato <guilherme.stern@usp.br>
2024-09-25 16:10:33 -0300

    site pronto

--- staticpages/views.py
+++ staticpages/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello World!<h1>")
+    context = {}
+    return render(request, 'staticpages/index.html', context)
 
+def about(request):
+    context = {}
+    return render(request, 'staticpages/about.html', context)
